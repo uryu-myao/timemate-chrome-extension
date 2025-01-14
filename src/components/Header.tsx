d519b3d3528205ecbf641b6f2c058698b2f93664
uryu <myao.jpn@gmail.com>
2025-01-14 15:44:46 +0900

    add scss

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import '../styles/Header.scss';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-inner">
+        <div className="header-logo"></div>
+        <div className="header-btns">
+          <button className="header-btn"></button>
+          <button className="header-btn"></button>
+          <button className="header-btn"></button>
+        </div>
+      </div>
+    </header>
+  );
+};
+export default Header;
---
+++ src/components/Timezone.tsx
@@ -0,0 +1,7 @@
+import '../styles/Timezone.scss';
+
+const Timezone = () => {
+  return <div className="timezone">Timezone</div>;
+};
+
+export default Timezone;
