10b66493963b923ddf8a285cecfc3c3281bb2017
uryu <myao.jpn@gmail.com>
2025-02-02 14:54:57 +0900

    add sub text

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -2,13 +2,18 @@ import '@styles/Searchbar.scss';
 
 const Searchbar = () => {
   return (
-    <div className="header-searchbar">
-      <input
-        className="header-searchbar__input"
-        type="text"
-        placeholder="Search..."
-      />
-    </div>
+    <>
+      <div className="header-searchbar">
+        <input
+          className="header-searchbar__input"
+          type="text"
+          placeholder="Search..."
+        />
+      </div>
+      <p className="timezone-subtext">
+        For city time reference only. Not for precise timekeeping.
+      </p>
+    </>
   );
 };
 
